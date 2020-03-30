8d790144644b9e6300a430e4a782b3f63dd182e5
Virksaabnavjot <virksaabnavjot@gmail.com>
2020-03-30 03:29:00 +0100

    Percentage Calculator Android App

---
+++ android_app/PercentageCalculator/app/src/main/java/com/mrvirk/calculator/percentage/MainActivity.java
@@ -0,0 +1,67 @@
+package com.mrvirk.calculator.percentage;
+
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        webView = (WebView) findViewById(R.id.percentageView);
+
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setAppCacheEnabled(true);
+
+        webView.loadUrl("https://percentage-calculator.mrvirk.com");
+        webView.setWebViewClient(new WebViewClient(){
+                                     public void onReceivedError(WebView view, int errorCode, String description, String failingUrl){
+                                         webView.loadUrl("file:///android_asset/index.html");
+                                     }
+
+                                 }
+        );
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
